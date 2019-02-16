eae76b6bc427b68aa8d440bdb3440c8b6b4e1114
Vũ Cường <phantom.yi@hotmail.com>
2019-02-16 10:41:35 +0700

    Chưa đúng yêu cầu

---
+++ Reverse In Parentheses/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_In_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "123(bda)34";
+            int startIndex = a.IndexOf('(');
+            int endIndex = a.IndexOf(')');
+            List<string> stringReverse=new List<string>();
+
+            for (int i = startIndex+1; i < endIndex; i++)
+            {
+                stringReverse.Add(a[i].ToString());
+            }
+
+            List<string> arrReverse = new List<string>();
+            for (int i = stringReverse.Count()-1; i >= 0; i--)
+            {
+                arrReverse.Add(stringReverse[i]);
+            }
+
+            string newString = string.Join(",", stringReverse.ToArray()).Replace(",","");
+            string oldString = string.Join(",", arrReverse.ToArray()).Replace(",","");
+
+            Console.WriteLine(newString.ToString());
+            Console.WriteLine(oldString.ToString());
+
+            string b = a.Replace(newString.ToString(), oldString.ToString());
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
